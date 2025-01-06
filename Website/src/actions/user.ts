eb2cc673806cf814d9ca3483a1b41a1cb01b53ec
sushant-wayal <sushantssr12345@gmail.com>
2025-01-06 13:37:40 +0530

    Add resetRefreshToken function and enhance YouTube statistics error handling

--- Website/src/actions/user.ts
+++ Website/src/actions/user.ts
@@ -97,3 +97,27 @@ export const updateYoutubeRefreshToken = async (id: string, refreshToken: string
     }
   });
 }
+
+export const resetRefreshToken = async (id: string) => {
+  const session = await auth();
+  if (!session || !session.user || !session.user.id) {
+    throw new Error("User not authenticated");
+  }
+  await db.user.update({
+    where: {
+      id,
+    },
+    data: {
+      youtubeRefreshToken: null
+    }
+  });
+  await db.notification.create({
+    data: {
+      title: "YouTube account disconnected",
+      message: "Your YouTube account has been disconnected",
+      type: "YOUTUBE_REFRESH",
+      senderProjectRole: "SYSTEM",
+      toUserId: id
+    }
+  });
+}
