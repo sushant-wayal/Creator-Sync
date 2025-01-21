4d64fd6e5882efa1385a92386745a7aab224fed0
Sushant Wayal <120119633+sushant-wayal@users.noreply.github.com>
2025-01-21 17:08:22 +0530

    Delete .next directory

--- .next/server/interception-route-rewrite-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__INTERCEPTION_ROUTE_REWRITE_MANIFEST="[{\"source\":\"/signin\",\"destination\":\"/(.)signin\",\"has\":[{\"type\":\"header\",\"key\":\"Next-Url\",\"value\":\"(?:\\\\/(.*))?[\\\\/#\\\\?]?\"}]}]"
